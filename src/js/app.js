e6433bf056dbf4633f7450cd6caded46d09ebfda
Jarvis Mercer <ams.h4ck3r@gmail.com>
2019-05-25 03:45:27 +0430

    remove callback

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+  // particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
+  //   console.log('callback - particles.js config loaded');
+  // });
   
